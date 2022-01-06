305f0e3742125655292a4b8b51c7dcdd2ce2890e
trongnsiwa <trongnsiwa79@gmail.com>
2022-01-06 10:45:23 +0700

    Add tRPC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,18 @@
+import { trpc } from "@/utils/trpc";
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const { data, isLoading } = trpc.useQuery(["hello", { text: "Theo" }]);
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (data) return <div>{data.greeting}</div>;
+
   return (
-    <div className="h-screen w-screen flex flex-col justify-center relative">
+    <div className="h-screen w-screen flex flex-col justify-center relative items-center">
       <div className="text-2xl text-center">Which Pokémon is Rounder?</div>
       <div className="p-2" />
-      <div className="border rounded p-8 flex justify-between max-w-2xl items-center flex-col">
+      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
         <div className="w-16 h-16 bg-red-200" />
         <div className="p-8">Vs</div>
         <div className="w-16 h-16 bg-red-200" />
