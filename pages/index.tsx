173adab5e86a36e9740ffcaed97f3115ae20fb3b
trongnsiwa <trongnsiwa79@gmail.com>
2022-01-06 10:03:39 +0700

    basic UI structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,16 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return <div className="text-2xl font-bold">Hello World</div>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center">
+      <div className="text-2xl text-center">Which Pokémon is Rounder?</div>
+      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+        <div className="w-16 h-16 bg-red-200" />
+        <div className="p-8">Vs</div>
+        <div className="w-16 h-16 bg-red-200" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
